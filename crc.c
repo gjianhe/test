7f9b037253bb8dfd702be5f435e6dc40ded38507
guanjianhe <51358787+gjianhe@users.noreply.github.com>
2019-07-19 14:32:41 +0800

    Update crc.c

--- crc.c
+++ crc.c
@@ -6,8 +6,11 @@ static uint16_t CrcValueCalc(const uint8_t* Data, int length)
     while (length --)
     {
         crcValue ^= (uint16_t) * (Data ++);
-
+        #if 0
         for (i = 7; i >= 0; i --)
+        #else
+        for (i = 0; i < 8; i++)
+        #endif
         {
             crcValue = (crcValue & 0x0001) ? ((crcValue >> 1) ^ 0xa001) : (crcValue >> 1);
         }
